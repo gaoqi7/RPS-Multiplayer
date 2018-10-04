d834fa6d6021d4acb537a1f70eb3ac74a4f2b901
administrator <administrator@administrators-MacBook-Pro.local>
2018-10-04 15:26:31 -0700

    Finally, Game part works:)

--- assets/script/game.js
+++ assets/script/game.js
@@ -11,61 +11,126 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var localRedShoot = "";
 var localBlueShoot = "";
-var redState = false;
-var blueState = false;
+var redBtnClicked = false;
+var blueBtnClicked = false;
+var result = "";
+// Initial Database
+database.ref("/red").set({
+  shoot: "",
+  btn: false
+});
+database.ref("/blue").set({
+  shoot: "",
+  btn: false
+});
+
+database.ref("/result").set({
+  output: ""
+});
+
+//Create an environment for retrieving the data
+database.ref().on("value", function(ss) {
+  var rr = ss.val().red.btn;
+  if (rr) {
+    $(".red").attr("disabled", "disabled");
+  }
+  if (rr === false) {
+    $(".red").removeAttr("disabled");
+  }
+  var bb = ss.val().blue.btn;
+  if (bb) {
+    $(".blue").attr("disabled", "disabled");
+  }
+  if (rr === false) {
+    $(".blue").removeAttr("disabled");
+  }
+});
+
+// If Red Button Clicked ...
 $(".red").on("click", function() {
-  database.ref("/playerRed").set({
-    shoot: $(this).text()
+  database.ref("/red").update({
+    shoot: $(this).text(),
+    //Use btn to broadcast the info that someone has picked red.
+    btn: true
   });
-  localRedShoot = $(this).text();
-  redState = true;
-  console.log("red choose " + localRedShoot);
-  $(".red").attr("disabled", "disabled");
+  // before run whoWin, make sure all the data used by whoWin() are coming from database. not from local!
+  database.ref("/red").on("value", function(snapshot) {
+    redBtnClicked = snapshot.val().btn;
+    localRedShoot = snapshot.val().shoot;
+  });
+  database.ref("/blue").on("value", function(snapshot) {
+    blueBtnClicked = snapshot.val().btn;
+    localBlueShoot = snapshot.val().shoot;
+  });
   whoWin();
 });
 
-$(".blue").click(function() {
-  database.ref("/playerBlue").set({
-    shoot: $(this).text()
+$(".blue").on("click", function() {
+  database.ref("/blue").update({
+    shoot: $(this).text(),
+    btn: true
   });
-  localBlueShoot = $(this).text();
-  $(this).addClass("done");
-  blueState = true;
-  console.log("blue choose " + localBlueShoot);
-  $(".blue").attr("disabled", "disabled");
-
+  database.ref("/red").on("value", function(snapshot) {
+    redBtnClicked = snapshot.val().btn;
+    localRedShoot = snapshot.val().shoot;
+  });
+  database.ref("/blue").on("value", function(snapshot) {
+    blueBtnClicked = snapshot.val().btn;
+    localBlueShoot = snapshot.val().shoot;
+  });
   whoWin();
 });
 
 function whoWin() {
-  console.log(redState);
-  console.log(blueState);
-
-  if (blueState && redState) {
+  if (blueBtnClicked && redBtnClicked) {
     if (
       localRedShoot === "r" ||
       localRedShoot === "p" ||
       localRedShoot === "s"
     ) {
       if (localRedShoot === "r" && localBlueShoot === "s") {
-        alert("Red Win");
+        //adding TimeStamp can make database record always change,then I can always do something after record updated.
+        database.ref("/result").set({
+          output: "Red Win",
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       } else if (localRedShoot === "r" && localBlueShoot === "p") {
-        alert("Red Lose");
+        database.ref("/result").set({
+          output: "Blue Win",
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       } else if (localRedShoot === "s" && localBlueShoot === "r") {
-        alert("Red Lose");
+        database.ref("/result").set({
+          output: "Blue Win",
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       } else if (localRedShoot === "s" && localBlueShoot === "p") {
-        alert("Red Win");
+        database.ref("/result").set({
+          output: "Red Win",
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       } else if (localRedShoot === "p" && localBlueShoot === "r") {
-        alert("Red Win");
+        database.ref("/result").set({
+          output: "Red Win",
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       } else if (localRedShoot === "p" && localBlueShoot === "s") {
-        alert("Red Lose");
+        database.ref("/result").set({
+          output: "Blue Win",
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       } else if (localRedShoot === localBlueShoot) {
-        alert("ties");
+        database.ref("/result").set({
+          output: "Ties",
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       }
     }
-    redState = false;
-    blueState = false;
-    $(".red").removeAttr("disabled");
-    $(".blue").removeAttr("disabled");
   }
 }
+database.ref("/result").on("value", function(snapshot) {
+  $("#result").text(snapshot.val().output);
+  // Reset Button State in Database
+  database.ref("/red").update({ btn: false });
+  database.ref("/blue").update({ btn: false });
+});
